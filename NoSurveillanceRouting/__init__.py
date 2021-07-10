cf63302938be53fc55a0c283664114bd1db8078a
Leo Maroni <git@em0lar.de>
2021-07-10 20:22:42 +0200

    Remove unneeded directories/files

--- NoSurveillanceRouting/__init__.py
+++
@@ -1,9 +0,0 @@
-import flask
-from flask import Flask
-
-app = Flask(__name__)
-
-
-@app.route("/")
-def start_site():
-    return flask.render_template("start.html")
--- NoSurveillanceRouting/wsgi.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-
-from . import app
-
-if __name__ == "__main__":
-    app.run()
