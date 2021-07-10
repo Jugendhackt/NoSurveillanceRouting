f79733b6755c6af55a7941628cc48b4db6a71e55
Arthent42 <hendrik-buerger@web.de>
2021-07-10 12:12:09 +0200

    Added frontend folder with first stage of the map

---
+++ frontend/scripts/map.js
@@ -0,0 +1,5 @@
+var mymap = L.map('map').setView([57.74, 11.94], 11);
+
+L.tileLayer( 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(mymap);
