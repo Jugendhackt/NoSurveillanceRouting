4639ae91486e23cbb18d01160eeda12ff8be19a8
Arthent42 <hendrik-buerger@web.de>
2021-07-10 14:50:33 +0200

    Enlarged map; added bootstrap navbar; added requirements.txt

--- frontend/scripts/map.js
+++ frontend/scripts/map.js
@@ -1,5 +1,11 @@
-var mymap = L.map('map').setView([57.74, 11.94], 11);
+var mymap = L.map('map').setView([53.55, 9.99], 12);
 
 L.tileLayer( 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(mymap);
+
+var clr = "#ff2100";
+L.rectangle([[90, 180], [-90, 9.9826]], {color: clr, weight: 0}).addTo(mymap);
+L.rectangle([[90, 9.9604], [53.5688, 9.9826]], {color: clr, weight: 0}).addTo(mymap);
+L.rectangle([[90, 9.9604], [-90, -180]], {color: clr, weight: 0}).addTo(mymap);
+L.rectangle([[53.5606, 9.9604], [-90, 9.9826]], {color: clr, weight: 0}).addTo(mymap);
