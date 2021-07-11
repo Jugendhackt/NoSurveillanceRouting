7e72eacb3edc47211e62dd3db7b82bb74f254d35
Moritz 'e1mo' Fromm <git@e1mo.de>
2021-07-11 11:22:28 +0200

    Add more performant parser

--- osrm_profiles/parser.py
+++ osrm_profiles/parser.py
@@ -19,42 +19,74 @@ cameras = []
 marked_nodes = []
 marked_nodes_file = 'data/surveilled_nodes.txt'
 
+parsed_noses = []
+
+def is_node_start(line):
+    return '<node' in line
+
+def is_node_ended(line):
+    return '</node' in line or (is_node_start(line) and ' />' in line )
+
+def is_surveillance_node(node):
+    for line in node:
+        if 'k="man_made" v="surveillance"' in line:
+            return True
+
+    return False
+
+def parse_surveillance_node(node):
+    part_names = {'uid': 'int', 'id': 'int', 'lat': 'float', 'lon': 'float'}
+    header = node[0]
+    _parts = header.split(' ')
+    parts = {}
+    for pn in part_names:
+        for p in _parts:
+            if p.startswith(f'{pn}=\"'):
+                _ps = p.split('"')
+                val = _ps[1]
+                val_type = part_names[pn]
+                if val_type == 'int':
+                    val = int(val)
+                elif val_type == 'float':
+                    val = float(val)
+
+                parts[pn] = val
+
+    return parts
+
 print('Loading data...')
 
 only_nodes = SoupStrainer("node")
 
 try:
     with open(in_file) as fp:
-        soup = BeautifulSoup(fp, 'xml', parse_only=only_nodes)
+        node = []
+        node_startet = False
+        node_ongoing = False
+        for _line in fp:
+            line = str(_line).strip().replace('\n', '')
+            if not node_ongoing:
+                node_started = is_node_start(line)
+                if not node_started:
+                    continue
+
+            node.append(line)
+            node_ongoing = not is_node_ended(line)
+
+            if not node_ongoing:
+                if is_surveillance_node(node):
+                    cameras.append(parse_surveillance_node(node))
+                node = []
+
 except FileNotFoundError:
     print(f'{in_file} was not found. Please place it there and run again.')
     exit(1)
 
 print('Data loaded')
 
-all_nodes = soup.find_all('node', recursive=False)
-all_nodes_len = len(all_nodes)
-surveilled_nodes = []
-
-print(f'Filtering {all_nodes_len} nodes...')
-
-for n in all_nodes:
-    man_made_surveillange = n.find('tag', k='man_made', v='surveillance', recursive=False)
-    if man_made_surveillange:
-        surveilled_nodes.append(n)
-
-print(f'Found {len(surveilled_nodes)} surveillance nodes.')
-print('Updating dataset...')
-
-for node in tqdm(surveilled_nodes):
-    lat = node.get('lat')
-    lon = node.get('lon')
-
-    camera = {
-        'lat': lat,
-        'lon': lon,
-    }
-    cameras.append(camera)
+for camera in tqdm(cameras):
+    lat = camera['lat']
+    lon = camera['lon']
 
     r = requests.get(f'{osrm_api}/nearest/v1/walking/{lon},{lat}?number={nearest_number}')
     data = r.json()
